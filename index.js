8658337266851c55f1e08797940c738f8758e120
unknown <jwall25@colostate.edu>
2021-11-16 23:39:51 -0700

    11/16 Getting HOE 11 Done

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const cors = require("cors");
+
+const bcrypt = require("bcryptjs");
 
 const db = require("./dbConnectExec.js");
+const walkConfig = require("./config.js");
+
+const auth = require("./middleware/authenticate");
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log("App is running on Port 5000");
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
+//azurwebsites.net, colostate.edu
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`App is running on Port ${PORT}`);
 });
 
 app.get("/hi", (req, res) => {
@@ -18,6 +32,166 @@ app.get("/", (req, res) => {
 
 // app.post();
 // app.put();
+
+app.post("/walkthroughs", auth, async (req, res) => {
+  try {
+    let gameFK = req.body.gameFK;
+    let title = req.body.title;
+    let summary = req.body.summary;
+    let rating = req.body.rating;
+    let vidleng = req.body.videolength;
+
+    if (
+      !gameFK ||
+      !summary ||
+      !title ||
+      !Number.isInteger(rating) ||
+      !Number.isInteger(vidleng)
+    ) {
+      return res.status(400).send("bad request");
+    }
+
+    summary = summary.replace("'", "''");
+    // console.log("summary", summary);
+
+    // console.log("here is the creator", req.creator);
+
+    let insertQuery = `INSERT INTO Walkthrough(Title,Summary,Rating,VideoLength,GameFK,CreatorFK)
+    OUTPUT inserted.WalkPK, inserted.Title, inserted.Summary, inserted.Rating, inserted.VideoLength, inserted.GameFK
+    VALUES('${title}', '${summary}', '${rating}', '${vidleng}', '${gameFK}', '${req.creator.CreatorPK}')`;
+
+    let insertedWalk = await db.executeQuery(insertQuery);
+
+    console.log("inserted walkthrough", insertedWalk);
+
+    res.status(201).send(insertedWalk[0]);
+  } catch (err) {
+    console.log("error in POST /walkthroughs", err);
+    res.status(500).send();
+  }
+});
+
+app.get("/creators/me", auth, (req, res) => {
+  res.send(req.creator);
+});
+
+app.post("/creators/login", async (req, res) => {
+  console.log("/creators/login called", req.body);
+  // 1. data validation
+
+  let email = req.body.email;
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if (!email || !password || !username) {
+    return res.status(400).send("bad Request");
+  }
+
+  // 2. check user exists
+
+  let query = `SELECT *
+  From Creator
+  Where Email = '${email}'`;
+
+  let result;
+
+  try {
+    result = await db.executeQuery(query);
+  } catch (myError) {
+    console.log("error in /creator/login", myError);
+    return res.status(500).send();
+  }
+
+  console.log(result);
+
+  if (!result[0]) {
+    return res.status(401).send("Invalid User Credentials.");
+  }
+  // 3. check user password
+
+  let user = result[0];
+
+  if (!bcrypt.compareSync(password, user.Password)) {
+    console.log("Invalid Password");
+    return res.status(401).send("Invalid User Credentials");
+  }
+
+  // 4. if good, generate token
+
+  let token = jwt.sign({ pk: user.CreatorPK }, walkConfig.JWT, {
+    expiresIn: "60 minutes",
+  });
+
+  // console.log("token", token);
+
+  // 5. save token in database and send response back
+
+  let setTokenQuery = `UPDATE Creator
+  SET Token = '${token}'
+  WHERE CreatorPK = ${user.CreatorPK}`;
+
+  try {
+    await db.executeQuery(setTokenQuery);
+    res.status(200).send({
+      token: token,
+      user: {
+        NameFirst: user.NameFirst,
+        NameLast: user.NameLast,
+        Username: user.Username,
+        Email: user.Email,
+        CreatorPK: user.CreatorPK,
+      },
+    });
+  } catch (myError) {
+    console.log("error in setting user token", myError);
+    res.status(500).send();
+  }
+});
+
+app.post("/creators", async (req, res) => {
+  // res.send("/creators called");
+
+  // console.log("request body", req.body);
+
+  let nameFirst = req.body.nameFirst;
+  let nameLast = req.body.nameLast;
+  let username = req.body.username;
+  let email = req.body.email;
+  let password = req.body.password;
+
+  if (!nameFirst || !nameLast || !email || !password || !username) {
+    return res.status(400).send("Bad Request");
+  }
+
+  nameFirst = nameFirst.replace("'", "''");
+  nameLast = nameLast.replace("'", "''");
+
+  let emailCheckQuery = `SELECT Email
+  FROM Creator
+  WHERE Email = '${email}'`;
+
+  let existingUser = await db.executeQuery(emailCheckQuery);
+
+  // console.log("existing user", existingUser);
+
+  if (existingUser[0]) {
+    return res.status(409).send("duplicate email");
+  }
+
+  let hashedPW = bcrypt.hashSync(password);
+
+  let insertQuery = `Insert into Creator(NameFirst,NameLast,Username,Email,Password)
+  VALUES('${nameFirst}','${nameLast}','${username}','${email}','${hashedPW}')`;
+
+  db.executeQuery(insertQuery)
+    .then(() => {
+      res.status(201).send();
+    })
+    .catch((err) => {
+      console.log("error in POST /contact", err);
+      res.status(500).send();
+    });
+});
 
 app.get("/games", (req, res) => {
   //res.send("games api running");
---
+++ middleware/authenticate.js
@@ -0,0 +1,43 @@
+const jwt = require("jsonwebtoken");
+const walkConfig = require("../config.js");
+const db = require("../dbConnectExec.js");
+
+const auth = async (req, res, next) => {
+  // console.log("in the middleware", req.header("Authorization"));
+  // next();
+
+  try {
+    //1. decode token
+
+    let myToken = req.header("Authorization").replace("Bearer ", "");
+    // console.log(myToken);
+
+    let decoded = jwt.verify(myToken, walkConfig.JWT);
+    console.log(decoded);
+
+    let creatorPK = decoded.pk;
+
+    //2. compare token with database
+
+    let query = `SELECT CreatorPK, NameFirst, NameLast, Username, Email
+    From Creator
+    Where CreatorPK = ${creatorPK} and Token = '${myToken}'`;
+
+    let returnedUser = await db.executeQuery(query);
+
+    console.log("returned user", returnedUser);
+
+    //3. save user info in request
+
+    if (returnedUser[0]) {
+      req.creator = returnedUser[0];
+      next();
+    } else {
+      return res.status(401)("Invalid credentials");
+    }
+  } catch (err) {
+    return res.status(401).send("Invalid Credentials");
+  }
+};
+
+module.exports = auth;
